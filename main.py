846f251371397f4aa789382088fe3eb4e0dc3c8f
Derek Baker <derek.baker89@gmail.com>
2021-10-29 11:33:46 +0100

    passed date column conversion test

--- main.py
+++ main.py
@@ -11,24 +11,28 @@ import pandas as pd
 app = dash.Dash(__name__)
 server = app.server
 
-def convert_from_CSV_to_DF(csv_filepath):
+
+def extract_road_casualty_statistics(csv_filepath):
     """Take in a CSV filepath and return a dataframe"""
     converted_df = pd.read_csv(csv_filepath, low_memory=False)
+    converted_df['date'] = pd.to_datetime(converted_df['date'], format="%d/%m/%Y")
     return converted_df
 
 
-df = convert_from_CSV_to_DF('data/dft-road-casualty-statistics-accident-2020.csv')
-df['date']= pd.to_datetime(df['date'], format="%d/%m/%Y")
+df = extract_road_casualty_statistics(
+    'data/dft-road-casualty-statistics-accident-2020.csv')
+
 
 # https://pandas.pydata.org/docs/reference/api/pandas.Series.to_frame.html#pandas-series-to-frame
-number_of_accidents_per_day = df.groupby(['date']).size().to_frame('number_of_accidents')
+number_of_accidents_per_day = df.groupby(
+    ['date']).size().to_frame('number_of_accidents')
 
 
 fig = px.line(
     number_of_accidents_per_day,
-    x = number_of_accidents_per_day.index,
-    y = "number_of_accidents",
-    title = "No. of road accidents per day in 2020",
+    x=number_of_accidents_per_day.index,
+    y="number_of_accidents",
+    title="No. of road accidents per day in 2020",
     labels={
         "number_of_accidents": "number of accidents"
     },
@@ -49,8 +53,8 @@ app.layout = html.Div([
         )
     ]),
     html.Div([
-    dcc.Graph(figure=fig)
-])
+        dcc.Graph(figure=fig)
+    ])
 ])
 
 if __name__ == '__main__':
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,17 +1,16 @@
 import pytest
 import main
-from pandas.testing import assert_frame_equal
-from io import StringIO
 import pandas as pd
+from datetime import date
+
+csv = 'data/dft-road-casualty-statistics-accident-2020.csv'
+df = main.extract_road_casualty_statistics(csv)
 
 
 def test_convert_from_CSV_to_DF():
-    # create dummy csv data
-    dummy_csv = 'data/dft-road-casualty-statistics-accident-2020.csv'
+    assert isinstance(df, pd.DataFrame)
+    assert len(df) == 91199
 
-    actual_df = main.convert_from_CSV_to_DF(dummy_csv)
 
-    assert isinstance(actual_df, pd.DataFrame)
-    assert len(actual_df) == 91199
-
-# test date formatting function
+def test_date_column_is_converted_to_datetime():
+    assert df.at[0, 'date'] == date(2020, 2, 4)
